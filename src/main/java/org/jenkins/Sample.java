cb392a5500f55d070203b1331f34b6f1c70d82ba
Alok Panigrahy <panigrahyalok2501@gmIL.com>
2022-07-13 23:16:19 +0530

    First commit

---
+++ src/main/java/org/jenkins/Sample.java
@@ -0,0 +1,11 @@
+package org.jenkins;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Maven");
+		// TODO Auto-generated method stub
+
+	}
+
+}
